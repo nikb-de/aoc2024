e411c967438e30cc02809f701332ada1e23c8c1f
Nikita Bakanchev <bakanchevn@gmail.com>
2024-12-05 23:37:08 +0100

    Refactored day 2

--- src/main/scala/nikbde/Day2.scala
+++ src/main/scala/nikbde/Day2.scala
@@ -2,15 +2,51 @@ package nikbde
 
 object Day2 {
 
-  @main def day2_1(): Unit = {
 
-    val bufferedSource = io.Source.fromResource("day2.txt")
-    val resSum = bufferedSource.getLines().foldLeft(0)({
-      (row, x) =>
-        
-      val rowParse = x.split(" ")
-          .map(_.toInt)
-          .sliding(2)
+
+  @main def day2_m(): Unit = {
+    println(day2_1())
+    println(day2_2())
+  }
+
+  def day2_2(): Int = {
+    val bufferedSource = io.Source.fromResource("real_inputs/day2.txt")
+    val resSum = bufferedSource.getLines().count(checkRowCorrectness)
+    resSum
+  }
+
+ def day2_1(): Int = {
+
+    val bufferedSource = io.Source.fromResource("real_inputs/day2.txt")
+    val resSum = bufferedSource.getLines().count(str =>
+      val arr = str.split(" ").map(_.toInt).toList
+      checkOrder(arr)
+    )
+    resSum
+
+  }
+
+
+  private def generateLists[T](list: List[T]): List[List[T]] = {
+    list.zipWithIndex.map { case (_, index) =>
+      list.patch(index, Nil, 1)
+    }
+  }
+
+  private def checkRowCorrectness(str: String): Boolean = {
+    val rowList = str.split(" ")
+      .map(_.toInt)
+
+    val allLists = generateLists(rowList.toList)
+
+    allLists.map {
+      x => checkOrder(x)
+    }.reduce(_ || _)
+  }
+
+  def checkOrder(arr: List[Int]): Boolean = {
+    {
+        val rowParse = arr.sliding(2)
           .map(x => Tuple2(x(0) < x(1), Math.abs(x(1) - x(0))))
           .foldLeft(Set[Boolean](), true)({
             case (acc, x) => (
@@ -19,25 +55,11 @@ object Day2 {
             )
           })
 
-      val res = rowParse match {
-          case (set, false) => 0
-          case (s, true) => if (s.size == 1) 1 else 0
+        rowParse match {
+          case (set, false) => false
+          case (s, true) => if (s.size == 1) true else false
         }
-
-      row + res
-
-    })
-    val s =
-      """
-        |fdafds
-        |fdsafdsa
-        |fdsafdsa
-        |""".stripMargin
-    println(resSum)
-
-    println(s)
-
+    }
   }
-
-
 }
+
