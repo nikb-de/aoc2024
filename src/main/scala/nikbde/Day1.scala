c72effd9b9ef7b7f89ff4834a0120e27285f8f35
Nikita Bakanchev <bakanchevn@gmail.com>
2024-12-05 23:19:20 +0100

    Refactored day 1

--- src/main/scala/nikbde/Day1.scala
+++ src/main/scala/nikbde/Day1.scala
@@ -1,24 +1,35 @@
 package nikbde
 
 object Day1 {
-  def read_day11(): (List[Int], List[Int]) = {
+
+
+  @main def day1_m(): Unit = {
+    println(task1())
+    println(task2())
+  }
+
+  private def read_day11(): (List[Int], List[Int]) = {
 
     val bufferedSource = io.Source.fromResource("real_inputs/day1.txt")
 
     bufferedSource.getLines()
       .map(_.split("   ").take(2).map(_.toInt).toList)
       .foldLeft((List[Int](), List[Int]()))((acc, x) => (x(0) :: acc._1, x(1) :: acc._2))
-    
+
   }
 
-  @main def day11(): Unit = {
-    val tuples = read_day11()
 
+  private def task1(): Int = {
+    val tuples = read_day11()
     val sorted1 = tuples._1.sorted
     val sorted2 = tuples._2.sorted
-
-    println(sorted1.zip(sorted2).foldLeft(0)((acc, x) => acc + Math.abs(x._1 - x._2)))
+    sorted1.zip(sorted2).foldLeft(0)((acc, x) => acc + Math.abs(x._1 - x._2))
   }
 
+  private def task2(): Int = {
+    val tuples = read_day11()
+    val sort2Map = tuples._2.groupBy(identity).view.mapValues(_.size)
+    tuples._1.map(a => a * sort2Map.getOrElse(a, 0)).sum
+  }
 
 }
