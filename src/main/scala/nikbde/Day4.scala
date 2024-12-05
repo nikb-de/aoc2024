57c9a9b1325f22ed0327016c5d0322faf8268123
Nikita Bakanchev <bakanchevn@gmail.com>
2024-12-06 00:04:20 +0100

    Refactored day 4

--- src/main/scala/nikbde/Day4.scala
+++ src/main/scala/nikbde/Day4.scala
@@ -2,44 +2,59 @@ package nikbde
 
 object Day4 {
 
-  @main def day4_m(): Unit = {
-    val bufferedSource = io.Source.fromResource("day4.txt")
-    val arr_chr = bufferedSource.getLines().map(_.toArray).toArray
+  @main def day4_m: Unit =
+    println(day4_1())
+    println(day4_2())
 
-    val arr_width = arr_chr(0).length
-    val arr_height = arr_chr.length
 
-    val goodValues = List("XMAS", "SAMX")
+  def day4_2(): Int =
+  {
+    val bufferedSource = io.Source.fromResource("real_inputs/day4.txt")
+    val charGrid = bufferedSource.getLines().map(_.toArray).toArray
 
-    def checkGoodValues(seq: Seq[Char]): Boolean = goodValues.contains(seq.mkString)
+    val vals = List("MMASS", "SSAMM", "MSAMS", "SMASM")
 
-    val rows = for {
-      i <- 0 until arr_height
-      j <- 0 until arr_width - 3
-      if checkGoodValues(arr_chr(i).slice(j, j + 4))
-    } yield (i, j)
+    val allVals = for {
+      x <- charGrid.indices
+      y <- charGrid(x).indices
+      if x + 2 < charGrid.length && y + 2 < charGrid(x).length
+      str = charGrid(x)(y) + charGrid(x)(y + 2).toString + charGrid(x + 1)(y + 1).toString + charGrid(x + 2)(y) + charGrid(x + 2)(y + 2)
+      if vals.contains(str)
+    } yield (x, y)
 
-    val columns = for {
-      i <- 0 until arr_height - 3
-      j <- 0 until arr_width
-      if checkGoodValues(Seq(arr_chr(i)(j), arr_chr(i + 1)(j), arr_chr(i + 2)(j), arr_chr(i + 3)(j)))
-    } yield (i, j)
-
-    val diagonals1 = for {
-      i <- 0 until arr_height - 3
-      j <- 0 until arr_width - 3
-      if checkGoodValues(Seq(arr_chr(i)(j), arr_chr(i + 1)(j + 1), arr_chr(i + 2)(j + 2), arr_chr(i + 3)(j + 3)))
-    } yield (i, j)
-
-    val diagonals2 = for {
-      i <- 0 until arr_height - 3
-      j <- 3 until arr_width
-      if checkGoodValues(Seq(arr_chr(i)(j), arr_chr(i + 1)(j - 1), arr_chr(i + 2)(j - 2), arr_chr(i + 3)(j - 3)))
-    } yield (i, j)
-
-    val resXmas = rows.size + columns.size + diagonals1.size + diagonals2.size
-
-    println(resXmas)
+    allVals.size
   }
 
+  def day4_1(): Int = {
+    val bufferedSource = io.Source.fromResource("real_inputs/day4.txt")
+    val charGrid: Array[Array[Char]] = bufferedSource.getLines().map(_.toArray).toArray
+
+    val goodValues: Set[String] = Set("XMAS", "SAMX")
+    val directions: List[(Int, Int)] = List(
+      (0, 1),  // Horizontal
+      (1, 0),  // Vertical
+      (1, 1),  // Diagonal 1
+      (1, -1)  // Diagonal 2
+    )
+
+    def isValidCoordinate(x: Int, y: Int, dx: Int, dy: Int): Boolean =
+      (0 until 4).forall(step =>
+          x + step * dx >= 0 &&
+          x + step * dx < charGrid.length &&
+          y + step * dy >= 0 &&
+          y + step * dy < charGrid(x).length
+      )
+
+    def extractSequence(x: Int, y: Int, dx: Int, dy: Int): Seq[Char] =
+      (0 until 4).map(step => charGrid(x + step * dx)(y + step * dy))
+
+    val matches = for {
+      x <- charGrid.indices
+      y <- charGrid(x).indices
+      (dx, dy) <- directions
+      if isValidCoordinate(x, y, dx, dy) && goodValues.contains(extractSequence(x, y, dx, dy).mkString)
+    } yield (x, y)
+
+    matches.size
+  }
 }
